e993031527b59fd0c1fd952496de2981764915ac
wancat <wancat@pm.me>
2020-07-29 18:52:00 +0800

    define data structure

---
+++ media.go
@@ -0,0 +1,17 @@
+package main
+
+import "time"
+
+type Media struct {
+	ID        uint
+	CreatedAt time.Time
+
+	Name    string
+	Domain  string
+	Country string
+	Source  string
+	Note    string
+
+	Company   Company
+	CompanyID uint
+}
