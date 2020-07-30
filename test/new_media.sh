e3b86b266f28e132d7485d9ccc7f6952e28cdb13
wancat <wancat@pm.me>
2020-07-30 20:08:48 +0800

    Update test script
    remove id from new media

--- test/new_media.sh
+++ test/new_media.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 curl -d '{
-  "id": 2,
   "name": "Media Name",
   "domain": "example.com",
   "country": "TW",
-  "company_id": 2,
+  "company_id": 1,
   "source": "Wikipedia", 
   "note": "Note"
 }' http://localhost:8080/media
 
 curl -d '{
-  "id": 2,
   "name": "Media Name LONGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG",
   "domain": "example.com",
   "country": "TW",
